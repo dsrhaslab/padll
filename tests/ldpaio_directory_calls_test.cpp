27534aaccfe27f2f174ca9b990ceb106989bc934
rgmacedo <ricardomacedo13@gmail.com>
2021-03-08 18:04:00 +0000

    test-mkdir-call created

--- tests/ldpaio_directory_calls_test.cpp
+++ tests/ldpaio_directory_calls_test.cpp
@@ -5,10 +5,19 @@
 
 #include <iostream>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-void test_mkdir_call ()
+int test_mkdir_call (const char* pathname, mode_t mode)
 {
     std::cout << "Test mkdir call\n";
+    int result = ::mkdir (pathname, mode);
+
+    if (result != 0) {
+        std::cerr << "Error while creating directory (" << errno << ")\n";
+    }
+
+    return result;
 }
 
 // readdir: https://c-for-dummies.com/blog/?p=3246
@@ -18,18 +27,18 @@ void test_readdir_call (const std::string& pathname)
 }
 
 /**
- * test_opendir_call:
+ * test_opendir_closedir_call:
  *
- * Validation: the StatisticsEntry 'opendir' of the Statistics' container reserved for
- * directory-based calls (PosixPassthrough::m_m_dir_stats), should contain:
+ * Validation: the statistic entries 'opendir' and 'closedir' of the Statistics' container reserved
+ * for directory-based calls (PosixPassthrough::m_dir_stats), should contain:
  *  - {1, 0, 0} for valid paths (success);
  *  - {1, 0, 1} for invalid paths (error).
  * @param pathname
  * @return
  */
-int test_open_closedir_call (const char* pathname)
+int test_opendir_closedir_call (const char* pathname)
 {
-    std::cout << "Test opendir call\n";
+    std::cout << "Test opendir and closedir calls\n";
     DIR* folder = ::opendir (pathname);
 
     if (folder == nullptr) {
@@ -44,15 +53,17 @@ int test_open_closedir_call (const char* pathname)
     return result;
 }
 
-
 void test_rmdir_call ()
 {
-    std::cout << "Test rmdir call\n";
+    std::cout << "Test rmdir calls\n";
+
 }
+
+
 
 int main (int argc, char** argv)
 {
-
-    test_open_closedir_call (argv[1]);
+    int mkdir_result = test_mkdir_call (argv[1], 0777);
+    test_opendir_closedir_call (argv[1]);
     return 0;
 }
