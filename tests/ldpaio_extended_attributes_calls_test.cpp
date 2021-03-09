7551290a008ec04512a4a94923119025cb79a688
rgmacedo <ricardomacedo13@gmail.com>
2021-03-09 15:45:42 +0000

    ExtendedAttribute calls updated

--- tests/ldpaio_extended_attributes_calls_test.cpp
+++ tests/ldpaio_extended_attributes_calls_test.cpp
@@ -4,10 +4,15 @@
 **/
 
 #include <sys/xattr.h>
-#include <string>
+#include <cstring>
 #include <iostream>
 
-
+/**
+ * test_getxattr_call:
+ * @param path
+ * @param xattr
+ * @return
+ */
 int test_getxattr_call (const char* path, const char* xattr)
 {
     std::cout << "Test getxattr call (" << path << ", " << xattr << ")\n";
@@ -24,7 +29,7 @@ int test_getxattr_call (const char* path, const char* xattr)
         return -1;
     }
 
-    char* info = static_cast<char *>(malloc(info_size));
+    char* info = static_cast<char*> (malloc (info_size));
 
     int return_value = ::getxattr (path, xattr, info, info_size
         #if defined(__APPLE__)
@@ -40,15 +45,44 @@ int test_getxattr_call (const char* path, const char* xattr)
     return return_value;
 }
 
+/**
+ * test_setxattr_call:
+ * @param path
+ * @param xattr
+ * @param value
+ * @return
+ */
+int test_setxattr_call (const char* path, const char* xattr, const char* value)
+{
+    std::cout << "Test setxattr call (" << path << ", " << xattr << ", " << value << ")\n";
 
+    // set extended attribute value
+    int return_value = ::setxattr (path, xattr, value, std::strlen (value)
+        #if defined(__APPLE__)
+                    , 0, 0
+        #endif
+        );
+
+    if (return_value != 0) {
+        std::cerr << "Error while getting attribute (" << errno << ")\n";
+    }
+
+    return return_value;
+}
+
+/**
+ * test_listxattr:
+ * @param path
+ * @return
+ */
 int test_listxattr (const char* path)
 {
-    ssize_t buflen, keylen, vallen;
-    char *buf, *key, *val;
+    std::cout << "Test listxattr call (" << path << ")\n";
 
-    /*
-     * Determine the length of the buffer needed.
-     */
+    ssize_t buflen, keylen;
+    char *buf, *key;
+
+    // determine the length of the buffer needed.
     buflen = ::listxattr (path, nullptr, 0
         #if defined(__APPLE__)
                     , 0
@@ -68,10 +102,12 @@ int test_listxattr (const char* path)
             break;
     }
 
-    buf = static_cast<char *>(malloc(buflen));
+    // allocate size for buffer
+    buf = static_cast<char*> (malloc (buflen));
+    // list extended attribute elements of a given file
     buflen = ::listxattr (path, buf, buflen
         #if defined(__APPLE__)
-                    , 0
+            , 0
         #endif
         );
 
@@ -80,72 +116,34 @@ int test_listxattr (const char* path)
         return EXIT_FAILURE;
     }
 
-    // Loop over the list of zero terminated strings with the attribute keys. Use the remaining
-    // buffer length to determine the end of the list
+    // loop over the list of zero terminated strings with the attribute keys
     key = buf;
     while (buflen > 0) {
-        // Output attribute key
         std::cout << key << "\n";
-
-//        // Determine length of the value
-//        vallen = ::getxattr (path, key, NULL, 0
-//            #if defined(__APPLE__)
-//                        , 0
-//            #endif
-//            );
-//
-//        if (vallen == -1)
-//            perror("getxattr");
-//
-//        if (vallen > 0) {
-//
-//            /*
-//             * Allocate value buffer.
-//             * One extra byte is needed to append 0x00.
-//             */
-//            val = malloc(vallen + 1);
-//            if (val == NULL) {
-//                perror("malloc");
-//                exit(EXIT_FAILURE);
-//            }
-//
-//            /*
-//             * Copy value to buffer.
-//             */
-//            vallen = getxattr(argv[1], key, val, vallen);
-//            if (vallen == -1)
-//                perror("getxattr");
-//            else {
-//                /*
-//                 * Output attribute value.
-//                 */
-//                val[vallen] = 0;
-//                printf("%s", val);
-//            }
-//
-//            free(val);
-//        } else if (vallen == 0)
-//            printf("<no value>");
-//
-//        printf("\n");
-
-        /*
-         * Forward to next attribute key.
-         */
-        keylen = strlen(key) + 1;
+        keylen = std::strlen(key) + 1;
         buflen -= keylen;
         key += keylen;
     }
 
-    free(buf);
-    exit(EXIT_SUCCESS);
+    free (buf);
+    free (key);
 
+    return EXIT_SUCCESS;
 }
 
-int main(int argc, char *argv[])
+int main (int argc, char** argv)
 {
-    std::string path = "/tmp/foo";
-    test_listxattr (path.data());
+    if (argc == 1) {
+        std::string path = "/tmp/foo";
+        std::string xattr = "user.tmp";
+        std::string value = "xyz-value";
+
+        test_setxattr_call (path.data (), xattr.data (), value.data ());
+        test_listxattr (path.data ());
+        test_getxattr_call (path.data (), xattr.data ());
+    } else {
+        test_listxattr (argv[1]);
+    }
 }
 
 
