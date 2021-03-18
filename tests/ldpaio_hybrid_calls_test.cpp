6b4f273472fbb84b17041119c5f3a1afd8809c00
rgmacedo <ricardomacedo13@gmail.com>
2021-03-18 10:52:34 +0000

    ldpaio-test updated; created ldpaio-hybrid-calls-test

---
+++ tests/ldpaio_hybrid_calls_test.cpp
@@ -0,0 +1,62 @@
+/**
+ *   Written by Ricardo Macedo.
+ *   Copyright (c) 2021 INESC TEC.
+ **/
+
+#include <cstdio>
+#include <cstdlib>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dlfcn.h>
+#include <string>
+#include <iostream>
+
+#define BUFFER_SIZE 1024
+
+typedef ssize_t (*my_write_t) (int, const void*, size_t);
+
+int main (int argc, char* argv[])
+{
+    bool differentiate_calls = true;
+    void* lib { nullptr };
+    my_write_t write_function_ptr { nullptr };
+
+    std::cout << "--> " << differentiate_calls << "\n";
+
+    // enable/disable ld_preloaded functions
+    if (argc < 2) {
+        differentiate_calls = std::stol (std::string {argv[2]});
+        std::cout << "--> " << differentiate_calls << "\n";
+    }
+
+    if (differentiate_calls) {
+        lib = ::dlopen ("libc.so.6", RTLD_LAZY);
+        write_function_ptr = (my_write_t)dlsym (lib, "write");
+    } else {
+        write_function_ptr = ::write;
+    }
+
+    ssize_t read_bytes = 0;
+    ssize_t written_bytes = 0;
+    ssize_t written_bytes_stdout = 0;
+    char* buffer = static_cast<char*> (std::malloc (sizeof (char) * BUFFER_SIZE));
+
+    int fd_write = ::open ("tmp.txt", O_CREAT | O_TRUNC | O_RDWR, 0600);
+
+    while ((read_bytes = ::read (STDIN_FILENO, buffer, BUFFER_SIZE)) > 0) {
+        written_bytes += ::write (fd_write, buffer, read_bytes);
+        written_bytes_stdout += write_function_ptr (1, buffer, read_bytes);
+    }
+
+    close (fd_write);
+    delete (buffer);
+
+    std::cout << "Read bytes: " << read_bytes << "\n";
+    std::cout << "Written bytes (file): " << written_bytes << "\n";
+    std::cout << "Written bytes (stdout): " << written_bytes_stdout << "\n";
+
+    // close pointer to lib
+    if (differentiate_calls) ::dlclose (lib);
+
+    return 0;
+}
--- tests/ldpaio_test.cpp
+++ tests/ldpaio_test.cpp
@@ -11,29 +11,8 @@
 
 #define BUFFER_SIZE 1024
 
-typedef ssize_t (*my_write_t) (int, const void*, size_t);
-bool is_ld_preload_enabled = true;
-
-
-ssize_t write_wrapper (int fd, const void* buffer, size_t size) {
-    return ::write (fd, buffer, size);
-}
-
-
 int main (int argc, char* argv[])
 {
-
-void* lib = ::dlopen ("libc.so.6", RTLD_LAZY);
-    my_write_t write_function_ptr { nullptr };
-
-    if (is_ld_preload_enabled) {
-        write_function_ptr = (my_write_t)dlsym (lib, "write");
-    } else {
-//        write_function_ptr = write_wrapper;
-        write_function_ptr = ::write;
-    }
-
-
     ssize_t read_bytes = 0;
     ssize_t written_bytes = 0;
     char* buffer = static_cast<char*> (std::malloc (sizeof (char) * BUFFER_SIZE));
@@ -42,9 +21,7 @@ void* lib = ::dlopen ("libc.so.6", RTLD_LAZY);
 
     while ((read_bytes = ::read (STDIN_FILENO, buffer, BUFFER_SIZE)) > 0) {
         written_bytes += ::write (fd_write, buffer, read_bytes);
-//	((my_write_t)dlsym (lib, "write")) (1, buffer, read_bytes);
-  	write_function_ptr (1, buffer, read_bytes);
-    }
+   }
 
     close (fd_write);
     delete (buffer);
