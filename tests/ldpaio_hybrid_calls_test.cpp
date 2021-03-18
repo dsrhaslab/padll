65c211cc9f2cbe66d9c92a1791fbc5a05a914f72
rgmacedo <ricardomacedo13@gmail.com>
2021-03-18 11:10:35 +0000

    ldpaio-hybrid-calls test updated

--- tests/ldpaio_hybrid_calls_test.cpp
+++ tests/ldpaio_hybrid_calls_test.cpp
@@ -24,19 +24,22 @@ int main (int argc, char* argv[])
     std::cout << "--> " << differentiate_calls << "\n";
 
     // enable/disable ld_preloaded functions
-    if (argc < 2) {
-        differentiate_calls = std::stol (std::string {argv[2]});
+    if (argc > 1) {
+        differentiate_calls = std::stol (std::string {argv[1]});
         std::cout << "--> " << differentiate_calls << "\n";
     }
 
     if (differentiate_calls) {
         lib = ::dlopen ("libc.so.6", RTLD_LAZY);
         write_function_ptr = (my_write_t)dlsym (lib, "write");
+        std::cout << "dlopen and dlsym made ...\n";
     } else {
         write_function_ptr = ::write;
+        std::cout << "attributed plain ::write pointer\n";
     }
 
-    ssize_t read_bytes = 0;
+    ssize_t read_bytes;
+    ssize_t total_read_bytes = 0;
     ssize_t written_bytes = 0;
     ssize_t written_bytes_stdout = 0;
     char* buffer = static_cast<char*> (std::malloc (sizeof (char) * BUFFER_SIZE));
@@ -46,12 +49,13 @@ int main (int argc, char* argv[])
     while ((read_bytes = ::read (STDIN_FILENO, buffer, BUFFER_SIZE)) > 0) {
         written_bytes += ::write (fd_write, buffer, read_bytes);
         written_bytes_stdout += write_function_ptr (1, buffer, read_bytes);
+        total_read_bytes += read_bytes;
     }
 
     close (fd_write);
     delete (buffer);
 
-    std::cout << "Read bytes: " << read_bytes << "\n";
+    std::cout << "Read bytes: " << total_read_bytes << "\n";
     std::cout << "Written bytes (file): " << written_bytes << "\n";
     std::cout << "Written bytes (stdout): " << written_bytes_stdout << "\n";
 
