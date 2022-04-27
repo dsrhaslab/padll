a32bfc3ea90a379c84847555e3ac64530ddae377
rgmacedo <ricardomacedo13@gmail.com>
2022-04-27 19:11:17 +0000

    PaioIntegration test updated

--- tests/padll_paio_integration_test.cpp
+++ tests/padll_paio_integration_test.cpp
@@ -1,38 +1,15 @@
 #include <padll/options/options.hpp>
 #include <paio/interface/paio_instance.hpp>
 #include <paio/stage/paio_stage.hpp>
+#include <random>
 
 namespace padll::tests {
 
 class StageIntegrationTest {
 
-public:
-    StageIntegrationTest () = default;
-
-    ~StageIntegrationTest () = default;
-
-    std::shared_ptr<paio::PaioStage> initialize (const int& num_channels,
-        const bool& object_creation,
-        const std::string& stage_name,
-        const std::string& hsk_file,
-        const std::string& dif_file,
-        const std::string& enf_file,
-        const bool& execute_on_receive)
-    {
-        std::cout << "\n\n-------------------------------------------------------\n";
-        return std::make_shared<paio::PaioStage> (num_channels,
-            object_creation,
-            stage_name,
-            hsk_file,
-            dif_file,
-            enf_file,
-            execute_on_receive);
-    }
-
-    void test_stage_info_to_string (std::shared_ptr<paio::PaioStage> stage_ptr)
-    {
-        std::cout << stage_ptr->stage_info_to_string () << std::endl;
-    }
+private:
+    std::shared_ptr<paio::PaioStage> m_stage_ptr { nullptr };
+    std::unique_ptr<paio::PaioInstance> m_instance_ptr { nullptr };
 
     /**
      * set_env: Set environment variable with name 'env_name' and value 'env_value'.
@@ -42,14 +19,140 @@ public:
      */
     bool set_env (const std::string_view& env_name, const std::string_view& env_value)
     {
+        if (env_name.empty () || env_value.empty ()) {
+            auto message { __func__ + std::string (": env_name/env_value cannot be empty.") };
+            throw std::invalid_argument (message);
+        }
+
         auto return_value = ::setenv (env_name.data (), env_value.data (), 1);
         return (return_value == 0);
     }
+
+    /**
+     * generate_context_object: Generate a context object.
+     */
+    paio::core::Context generate_context_object (const int& workflow_range, const int& constant, const int& operation_type_range, const int& operation_context_range)
+    {
+        auto workflow_id = static_cast<long> (random () % workflow_range) * constant;
+        auto operation_type = static_cast<int> (random () % operation_type_range);
+        auto operation_context = static_cast<int> (random () % operation_context_range);
+
+        return paio::core::Context { workflow_id, operation_type, operation_context, 1, 1 };
+    }
+
+    /**
+     * submit_request:
+     */
+    void submit_request (int workflow_range, int constant, int operation_type_range, int operation_context_range, bool detailed_debug)
+    {
+        auto context_object = this->generate_context_object (workflow_range, constant, operation_type_range, operation_context_range);
+        
+        if (detailed_debug) {
+            std::cout << context_object.to_string () << std::endl;
+        }
+        
+        Result result {};
+        this->m_instance_ptr->enforce (context_object, result);
+
+        // validate ResultStatus
+        if (result.get_result_status () != paio::enforcement::ResultStatus::success) {
+            std::cerr << __func__  << ": enforce failed." << std::endl;
+        }
+        
+        if (detailed_debug) {
+            std::cout << "Result: " << result.to_string () << std::endl;
+        }
+    }
+
+    /**
+     * execute_worker:
+     */
+    void execute_worker (int worker_id, int iterations, int workflow_range, int constant, int operation_type_range, int operation_context_range, bool detailed_debug)
+    {
+        for (int i = 0; i < iterations; i++) {
+            this->submit_request (workflow_range, constant, operation_type_range, operation_context_range, detailed_debug);
+        }
+    }
+
+public:
+    /**
+     * StageIntegrationTest default constructor.
+     */
+    StageIntegrationTest () = default;
+
+    /**
+     * StageIntegrationTest parameterized constructor.
+     * This constructor will set the PAIO environment variable 'paio_env' to 'env_value'.
+     */
+    explicit StageIntegrationTest (const std::string_view& env_value)
+    {
+        // set environment variable to paio_env
+        this->set_env (options::option_paio_environment_variable_env, env_value);
+    }
+
+    /**
+     * StageIntegrationTest default destructor.
+     */
+    ~StageIntegrationTest () = default;
+
+    /**
+     * initialize: Initialize the PAIO data plane stage.
+     */
+    void initialize (const int& num_channels,
+        const bool& object_creation,
+        const std::string& stage_name,
+        const std::string& hsk_file,
+        const std::string& dif_file,
+        const std::string& enf_file,
+        const bool& execute_on_receive)
+    {
+        std::cout << "\n\n-------------------------------------------------------\n";
+        // create shared ptr to a PaioStage
+        this->m_stage_ptr = std::make_shared<paio::PaioStage> (num_channels,
+            object_creation,
+            stage_name,
+            hsk_file,
+            dif_file,
+            enf_file,
+            execute_on_receive);
+
+        this->m_instance_ptr = std::make_unique<paio::PaioInstance> (this->m_stage_ptr);
+    }
+
+    /**
+     * test_to_string: write to stdout StageInfo in string-based format of the stage_ptr object.
+     */
+    void test_to_string ()
+    {
+        std::cout << this->m_stage_ptr->stage_info_to_string () << std::endl;
+        std::cout << this->m_instance_ptr->to_string () << std::endl;
+    }
+
+    void spawn_workers (int num_workers, int iterations, int workflow_range, int constant, int operation_type_range, int operation_context_range, bool detailed_debug)
+    {
+        std::vector<std::thread> workers;
+        for (int i = 0; i < num_workers; i++) {
+            workers.emplace_back (std::thread (std::bind (&StageIntegrationTest::execute_worker, this, i, iterations, workflow_range, constant, operation_type_range, operation_context_range, detailed_debug)));
+        }
+
+        for (auto& worker : workers) {
+            worker.join ();
+        }
+    }
+
 };
 } // namespace padll::tests
 
 using namespace padll::tests;
 using namespace padll::options;
+
+void print_header ()
+{
+    std::this_thread::sleep_for (std::chrono::seconds (1));
+    std::cout << "\n\n-------------------------------------------------------\n";
+    std::cout << "PADLL - PAIO Data Plane Stage Integration Test\n";
+    std::cout << "-------------------------------------------------------\n";
+}
 
 int main ()
 {
@@ -70,14 +173,14 @@ int main ()
     std::string_view stage_env { padll::options::option_paio_environment_variable_env };
     std::string_view stage_env_value { "testing-environment" };
 
-    std::cout << "-------------------------------------------------------\n\n";
+    // print header to stdout
+    print_header ();
 
-    StageIntegrationTest test {};
-    std::this_thread::sleep_for (std::chrono::seconds (2));
+    // create testing object
+    StageIntegrationTest stage_test { stage_env_value };
 
-    test.set_env (stage_env, stage_env_value);
-
-    std::shared_ptr<paio::PaioStage> stage_ptr = test.initialize (num_channels,
+    // initialize PAIO stage
+    stage_test.initialize (num_channels,
         default_object_creation,
         stage_name,
         housekeeping_rules_path,
@@ -87,10 +190,17 @@ int main ()
 
     std::this_thread::sleep_for (std::chrono::seconds (1));
 
-    test.test_stage_info_to_string (stage_ptr);
+    stage_test.test_to_string ();
 
-    std::unique_ptr<paio::PaioInstance> instance_ptr
-        = std::make_unique<paio::PaioInstance> (stage_ptr);
+    int num_workers = 1;
+    int iterations = 10;
+    int workflow_range = 4;
+    int constant = 1000;
+    int operation_type_range = paio::core::posix_size;
+    int operation_context_range = paio::core::posix_meta_size;
+    bool debug = false;
+
+    stage_test.spawn_workers (num_workers, iterations, workflow_range, constant, operation_type_range, operation_context_range, debug);
 
     std::cout << "\n-------------------------------------------------------\n\n";
 
